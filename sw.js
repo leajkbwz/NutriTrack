24f0e3f0fd7677797f1cbe3f59ec20501ee469d4
leajkbwz <132353021+leajkbwz@users.noreply.github.com>
2026-03-25 15:58:16 +0100

    Defect sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
-const CACHE = 'nutritrack-v1';
-const ASSETS = ['/', '/index.html'];
-
+const CACHE = 'nutritrack-v3';
+const ASSETS = ['/NutriTrack/', '/NutriTrack/index.html'];
+ 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/NutriTrack/index.html')))
   );
 });
-
